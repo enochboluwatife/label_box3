3d76d610f8f900b24b5f0f63c1378540025a7936
enochboluwatife <82175382+enochboluwatife@users.noreply.github.com>
2024-12-27 07:03:08 +0100

    Update manage.py
    yhhhhhyhhh

--- manage.py
+++ manage.py
@@ -3,10 +3,13 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'labelbox_webapp.settings')
+
+    # Check for the PORT environment variable (defaults to 8000 if not set)
+    port = os.environ.get("PORT", "8000")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,8 +18,9 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
 
+    # Run the server, binding to all IPs (0.0.0.0) and using the correct port
+    execute_from_command_line([sys.argv[0], "runserver", f"0.0.0.0:{port}"])
 
 if __name__ == '__main__':
     main()
