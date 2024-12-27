8076f4cbca2571844e56d69c9f47d90085439667
enochboluwatife <82175382+enochboluwatife@users.noreply.github.com>
2024-12-27 13:33:41 +0100

    Update wsgi.py

--- labelbox_webapp/wsgi.py
+++ labelbox_webapp/wsgi.py
@@ -14,3 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'labelbox_webapp.settings')
 
 application = get_wsgi_application()
+
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'labelbox_webapp.settings')
+    port = os.environ.get("PORT", "8000")  # Default to port 8000 if PORT is not set
+    execute_from_command_line(["manage.py", "runserver", f"0.0.0.0:{port}"])
